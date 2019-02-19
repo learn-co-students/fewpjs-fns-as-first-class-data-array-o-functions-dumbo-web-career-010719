2fb5d8cc4af0b1bbf35ec15ed16fb7ba570caae1
github username <github email address>
2019-02-19 18:38:06 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,69 @@
+function wakeDog(dogName, dogBreed){
+	if (dogName === undefined){
+		dogName = "Byron";
+	}
+	if (dogBreed === undefined){
+		dogBreed = "poodle";
+	}
+  return (`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog(dogName, dogBreed){
+	if (dogName === undefined){
+		dogName = "Byron";
+	}
+	if (dogBreed === undefined){
+		dogBreed = "poodle";
+	}
+  return (`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed){
+	if (dogName === undefined){
+		dogName = "Byron";
+	}
+	if (dogBreed === undefined){
+		dogBreed = "poodle";
+	}
+  return (`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+function throwFrisbee(dogName, dogBreed){
+	if (dogName === undefined){
+		dogName = "Byron";
+	}
+	if (dogBreed === undefined){
+		dogBreed = "poodle";
+	}
+  return (`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed){
+	if (dogName === undefined){
+		dogName = "Byron";
+	}
+	if (dogBreed === undefined){
+		dogBreed = "poodle";
+	}
+  return (`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName, dogBreed){
+	if (dogName === undefined){
+		dogName = "Byron";
+	}
+	if (dogBreed === undefined){
+		dogBreed = "poodle";
+	}
+  return (`Unleash ${dogName} the ${dogBreed}`);
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(){
+	let i = 0
+	while (i < routine.length){
+		routine[i]();
+	}
+}
+
